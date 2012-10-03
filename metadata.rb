79280ad51aee9494cf3df7748fb55e332fcb1437
jtimberman <joshua@opscode.com>
2012-10-02 21:22:40 -0600

    rename readme in metadata.rb too

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,5 @@ maintainer       "Opscode, Inc."
 maintainer_email "sysadmin@opscode.com"
 license          "Apache 2.0"
 description      "Used by the Fast Start guides at wiki.opscode.com"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.4.0"
