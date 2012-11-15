ec6b8de327daa43403560a1204a2ac7645a3b959
jtimberman <joshua@opscode.com>
2012-11-15 16:24:10 -0700

    update maintainer email

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             "getting-started"
 maintainer       "Opscode, Inc."
-maintainer_email "sysadmin@opscode.com"
+maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Used by the Fast Start guides at wiki.opscode.com"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
